5fc283d689e8792726556ae7ea2dc62de63aec86
Kevin Lam <mtn013209@gmail.com>
2022-11-09 09:16:21 -0800

    footer placeholder done
    Co-authored-by: Kenny Johnson <papercircuit@users.noreply.github.com>

--- client/app/App.js
+++ client/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import Navbar from '../features/navbar/Navbar';
+import Footer from '../features/footer/Footer';
 import AppRoutes from './AppRoutes';
 
 const App = () => {
@@ -8,6 +8,7 @@ const App = () => {
     <div>
       <Navbar />
       <AppRoutes />
+      <Footer />
     </div>
   );
 };
--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { logout } from "../../app/store"
 
 const Navbar = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const logoutAndRedirectHome = () => {
-    dispatch(logout());
-    navigate('/login');
-  };
+    dispatch(logout())
+    navigate("/login")
+  }
 
   return (
     <div>
@@ -34,7 +34,7 @@ const Navbar = () => {
       </nav>
       <hr />
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
