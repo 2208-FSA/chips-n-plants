a571be0f874dfb1775b83aec473574164c16cb87
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2022-11-10 15:33:03 +0000

    Added router.post to api

--- server/api/Products.js
+++ server/api/Products.js
@@ -22,4 +22,15 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.send(newProduct);
+  } catch (err) {
+    console.log("Error in POST", err);
+  }
+});
+
+
+
 module.exports = router;
