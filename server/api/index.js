1626cff8124341d1ce724c239cb4715d04446c2e
Kevin Lam <mtn013209@gmail.com>
2022-11-11 20:59:13 -0800

    undo orderproducts api

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,6 @@ const router = require("express").Router()
 router.use("/products", require("./Products"))
 router.use("/users", require("./users"))
 router.use("/orders", require("./orders"))
-router.use("/orders_products", require("./ordersProducts"))
 
 router.use((req, res, next) => {
   const error = new Error("Not Found")
