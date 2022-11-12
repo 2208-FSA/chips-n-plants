419abe02ea912a1332dcc597933142f537afbfde
Kevin Lam <mtn013209@gmail.com>
2022-11-11 20:44:30 -0800

    one orders products route working

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,14 @@
-const router = require('express').Router()
+const router = require("express").Router()
 
-router.use('/products', require('./Products'))
-router.use('/users', require('./users'))
-router.use('/orders', require('./orders'))
-
+router.use("/products", require("./Products"))
+router.use("/users", require("./users"))
+router.use("/orders", require("./orders"))
+router.use("/orders_products", require("./ordersProducts"))
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
+  const error = new Error("Not Found")
   error.status = 404
   next(error)
-});
+})
 
 module.exports = router
